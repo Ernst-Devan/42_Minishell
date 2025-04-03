840ab6bf4aa08daea569bb1a14ac81767156c691
dernst <dernst@student.42lyon.fr>
2025-04-03 11:51:12 +0200

    Repush: Libft
    Signed-off-by: dernst <dernst@student.42lyon.fr>

---
+++ libs/Libft/srcs/str/ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dernst <dernst@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 15:06:52 by ernstdevan        #+#    #+#             */
+/*   Updated: 2025/03/21 10:37:35 by dernst           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+
+	str = malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (*s1)
+	{
+		str[i] = *s1;
+		s1++;
+		i++;
+	}
+	while (*s2)
+	{
+		str[i] = *s2;
+		s2++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
