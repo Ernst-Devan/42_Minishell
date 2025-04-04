eade05e398151e42718fc0ffeb1f7ddcc3247a2b
Nicolas Jooris <njooris@student.42lyon.fr>
2025-04-04 15:14:14 +0200

    creation file exec / patch main with while

--- src/minishell.c
+++ src/minishell.c
@@ -1,13 +1,23 @@
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "unistd.h"
 #include "minishell.h"
+#include "parse.h"
+
+void	minishell(char **env)
+{
+	char	**data;
+
+	while (1)
+    {
+		data = parse();
+        exec(data);
+    }
+}
 
 int main(int ac, char **argv, char **env)
 {
+	char	**parse;
     (void)ac;
     (void)argv;
-    parsing(env);
+	minishell(env);
+	return (0);
 }
