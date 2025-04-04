cda27a5279693245d786f88959964a4381f53452
Nicolas Jooris <njooris@student.42lyon.fr>
2025-04-04 15:39:26 +0200

    creation file : exec.c

--- src/exec.c
+++ src/exec.c
@@ -6,11 +6,18 @@
 /*   By: njooris <njooris@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 15:03:30 by njooris           #+#    #+#             */
-/*   Updated: 2025/04/04 15:05:43 by njooris          ###   ########.fr       */
+/*   Updated: 2025/04/04 15:37:08 by njooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	exec_src_bin()
+{
+	// execution du path avec le fichier binaire
+	return (0);
+}
+
 int	exec(char **parse)
 {
-	
+	(void)parse;
+	return (0);
 }
