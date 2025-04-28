09ad06f3d387006341d65f7ca9974d611b2f5135
Nicolas Jooris <njooris@student.42lyon.fr>
2025-04-28 15:47:16 +0200

    EXEC : echo.c : patch echo -n if string NULL

--- src/echo.c
+++ src/echo.c
@@ -6,7 +6,7 @@
 /*   By: njooris <njooris@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 15:02:38 by njooris           #+#    #+#             */
-/*   Updated: 2025/04/28 15:30:50 by njooris          ###   ########.fr       */
+/*   Updated: 2025/04/28 15:46:35 by njooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@
 int	echo(t_cmd command)
 {
 	if (ft_strncmp("-n", command.args[1], 3) == 0)
-		printf("%s", command.args[2]);
+	{
+		if (command.args[2])
+			printf("%s", command.args[2]);
+	}
 	else
 		printf("%s\n", command.args[1]);
 	return (0);
