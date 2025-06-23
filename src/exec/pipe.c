3e7db5a29e03a680ef6df99a9182d34dff86f07f
Nicolas Jooris <njooris@student.42lyon.fr>
2025-06-23 13:27:09 +0200

    pipe patch and makefile relink patch

--- src/exec/pipe.c
+++ src/exec/pipe.c
@@ -6,7 +6,7 @@
 /*   By: njooris <njooris@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 10:55:59 by njooris           #+#    #+#             */
-/*   Updated: 2025/06/23 11:56:37 by njooris          ###   ########.fr       */
+/*   Updated: 2025/06/23 13:17:22 by njooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,75 +30,6 @@ void	manage_close_in_pipe(t_cmd command, int in, int pipefd[2])
 		close(pipefd[1]);
 }
 
-void	manage_dup_pipe(t_cmd command, int pipefd[2], int in, t_pack_pipe pp)
-{
-	if (!command.path)
-		return ;
-	if (pipefd[0] != STDIN_FILENO && pipefd[0] != in)
-		close(pipefd[0]);
-	if (command.in != STDIN_FILENO && in != command.in)
-	{
-		close(in);
-		in = command.in;
-	}
-	if (command.out != STDOUT_FILENO && pipefd[1] != command.out)
-	{
-		close(pipefd[1]);
-		pipefd[1] = command.out;
-	}
-	if (dup2(pipefd[1], STDOUT_FILENO) == -1 || dup2(in, STDIN_FILENO) == -1)
-	{
-		perror("dup2 faild in usepipe");
-		free_table(pp.table);
-		free_lstr(pp.shell->env);
-		exit(1);
-	}
-	if (in != STDIN_FILENO)
-		close(in);
-	if (pipefd[1] != STDOUT_FILENO)
-		close(pipefd[1]);
-}
-
-int	use_pipe(t_cmd command, int in, int pipefd[2], t_pack_pipe pp)
-{
-	pid_t	pid;
-
-	pid = fork();
-	if (pid == -1)
-		return (perror("fork error in use pipe"), 1);
-	if (!command.path) // dernier ajout qui patch >1 | >1 | >1
-		close(pipefd[0]);
-	if (pid == 0)
-	{
-		if (!command.path)
-		{
-			close(pipefd[1]);
-			close_fd(pp.table);
-			free_table(pp.table);
-			free_lstr(pp.shell->env);
-			exit(0);
-		}
-		manage_dup_pipe(command, pipefd, in, pp);
-		if (command.type == 0)
-		{
-			signal(SIGQUIT, SIG_DFL);
-			signal(SIGPIPE, SIG_DFL);
-			execve(command.path, command.args, pp.shell->env);
-			free_table(pp.table);
-			free_lstr(pp.shell->env);
-			perror("Commande not found");
-			exit(1);
-		}
-		exec_builtins(command, &pp.shell->env, pp.shell, pp.table, pipefd);
-		close_fd(pp.table);
-		free_table(pp.table);
-		free_lstr(pp.shell->env);
-		exit(1);
-	}
-	manage_close_in_pipe(command, in, pipefd);
-	return (pid);
-}
-
 int	waiter(pid_t last_pid)
 {
 	int		status;
@@ -118,6 +49,83 @@ int	waiter(pid_t last_pid)
 	else if (WIFSIGNALED(last_status))
 		return (128 + WTERMSIG(last_status));
 	return (last_status);
+}
+
+int	exit_pipe(int	pipefd[2], t_pack_pipe pp)
+{
+	close(pipefd[0]);
+	close(pipefd[1]);
+	close_fd(pp.table);
+	free_table(pp.table);
+	free_lstr(pp.shell->env);
+	exit(0);
+}
+
+void	manage_dup_pipe(t_cmd command, int pipefd[2], int in, t_pack_pipe pp)
+{
+	if (pipefd[0] != STDIN_FILENO && pipefd[0] != in)
+		close(pipefd[0]);
+	if (command.in != STDIN_FILENO && in != command.in)
+	{
+		close(in);
+		in = command.in;
+	}
+	if (command.out != STDOUT_FILENO && pipefd[1] != command.out)
+	{
+		close(pipefd[1]);
+		pipefd[1] = command.out;
+	}
+	if (dup2(pipefd[1], STDOUT_FILENO) == -1 || dup2(in, STDIN_FILENO) == -1)
+	{
+		perror("dup2 faild in usepipe");
+		free_table(pp.table);
+		free_lstr(pp.shell->env);
+		close(in);
+		exit_pipe(pipefd, pp);
+	}
+	if (in != STDIN_FILENO)
+		close(in);
+	if (pipefd[1] != STDOUT_FILENO)
+		close(pipefd[1]);
+}
+
+void	reset_signals()
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	signal(SIGPIPE, SIG_DFL);
+}
+
+int	use_pipe(t_cmd command, int in, int pipefd[2], t_pack_pipe pp)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		exit_pipe(pipefd, pp);
+		return (perror("fork error in use pipe"), 1);
+	}
+	if (pid == 0)
+	{
+		if (!command.path)
+			exit_pipe(pipefd, pp);
+		manage_dup_pipe(command, pipefd, in, pp);
+		if (command.type == 0)
+		{
+			reset_signals();
+			execve(command.path, command.args, pp.shell->env);
+			perror("Commande not found");
+		}
+		else
+			exec_builtins(command, &pp.shell->env, pp.shell, pp.table, pipefd);
+		close_fd(pp.table);
+		free_table(pp.table);
+		free_lstr(pp.shell->env);
+		exit(1);
+	}
+	manage_close_in_pipe(command, in, pipefd); // a refaire
+	return (pid);
 }
 
 int	ms_pipe(t_table table, t_shell *shell)
@@ -140,8 +148,8 @@ int	ms_pipe(t_table table, t_shell *shell)
 		if (i + 1 == table.cmd_len)
 			pipefd[1] = table.cmds[i].out;
 		val_return = use_pipe(table.cmds[i], save_in, pipefd, pack_pipe);
-		if (val_return == 1 || val_return == -1)
-			return (val_return);
+		if (val_return == -1)
+			return (1);
 		i++;
 	}
 	return (waiter(val_return));
